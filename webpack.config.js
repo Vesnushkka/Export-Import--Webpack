f6a66d0e25c1ee39315447212c4f68e677aa333a
Vesnushkka <natasha.kochneva@gmail.com>
2022-05-17 13:10:14 +0500

    Webpack config committed

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: "./src/index.js",
+    entry: path.resolve(__dirname, "src", "index.js"),
     output: {
-        path: path.resolve(__dirname, "./dist"),
+        path: path.resolve(__dirname, "dist"),
         filename: "[name].bundle.js",
-    }
-}
+    },
+    devServer: {
+        open: true,
+        compress: true,
+        hot: true,
+        port: 8080,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "src", "index.html"),
+        }),
+    ],
+};
